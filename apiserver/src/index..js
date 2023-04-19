2be53acd10c7589bba5b98b2347c4bba4b4a89e1
Arvindh <arvindhdevlps17@gmail.com>
2023-04-20 01:45:25 +0530

    React Components

---
+++ apiserver/src/index..js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExpenseEntryItemList from './components/ExpenseEntryItemList';
+
+ReactDOM.render(
+   <React.StrictMode>
+         <ExpenseEntryItemList />
+   </React.StrictMode>,
+   document.getElementById('root')
+);
